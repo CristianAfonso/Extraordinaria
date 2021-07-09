9dacf6dba7a139a4b556e674731a46661cb65e95
can97 <can97@DESKTOP-8FEJ6I4>
2021-07-09 10:15:22 +0100

    PrimerCommit

---
+++ src/extraordinaria2021/model/PatrimonialElement.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extraordinaria2021.model;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author can97
+ */
+public abstract class PatrimonialElement {
+    private int[][] localization;
+    private String name;
+    private String author;
+    private int year;
+    private String fabricationMethod;
+    private String material;
+    private String conservationState;
+    private ArrayList<String> photoGallery;
+    
+}
